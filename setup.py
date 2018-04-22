2d49ffbd67a3f6481bfc0264e2c052d745ad5f3c
Humberto Rocha <humrochagf@gmail.com>
2018-04-22 17:33:36 -0300

    Fix add translations to the build

--- setup.py
+++ setup.py
@@ -1,10 +1,40 @@
-from setuptools import find_packages, setup
+import os
+import subprocess
+
+from setuptools import setup
 
 try:
     import pypandoc
     README = pypandoc.convert('README.md', 'rst')
 except (IOError, ImportError):
     README = ''
+
+
+def create_mo_files():
+    data_files = []
+    localedir = 'locales'
+    po_dirs = [
+        localedir + '/' + l + '/LC_MESSAGES/'
+        for l in next(os.walk(localedir))[1]
+    ]
+
+    for d in po_dirs:
+        mo_files = []
+        po_files = [
+            f for f in next(os.walk(d))[2] if os.path.splitext(f)[1] == '.po'
+        ]
+
+        for po_file in po_files:
+            filename, extension = os.path.splitext(po_file)
+            mo_file = filename + '.mo'
+            msgfmt_cmd = 'msgfmt {} -o {}'.format(d + po_file, d + mo_file)
+            subprocess.call(msgfmt_cmd, shell=True)
+            mo_files.append(d + mo_file)
+
+        data_files.append((d, mo_files))
+
+    return data_files
+
 
 setup(
     name='lektor-creative-commons',
@@ -21,8 +51,5 @@ setup(
             'creative-commons = lektor_creative_commons:CreativeCommonsPlugin',
         ]
     },
-    packages=find_packages(),
-    package_data={
-        '': ['*.pot', '*.po', '*.mo'],
-    }
+    data_files=create_mo_files(),
 )
