a16ef973560ed6a8baa951612ec5b17d0cc8bff4
Martin Häcker <mhaecker@mac.com>
2018-06-24 19:36:14 +0200

    Allow removal of obsolete translations

--- bin/update_translations.sh
+++ bin/update_translations.sh
@@ -2,7 +2,7 @@
 
 POT_FILE=lektor_creative_commons/locales/messages.pot
 
-xgettext --no-location lektor_creative_commons/plugin.py --join-existing --output $POT_FILE
+xgettext --no-location lektor_creative_commons/plugin.py --output $POT_FILE
 
 for file in $(find . -name '*.po') ; do
     msgmerge --quiet --no-location --backup=none --update $file $POT_FILE
