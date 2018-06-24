a02d82f62fd98687fe522fb939ab8120c0fe210e
Martin Häcker <mhaecker@mac.com>
2018-06-24 19:24:34 +0200

    Add script to update translations

---
+++ bin/update_translations.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POT_FILE=lektor_creative_commons/locales/messages.pot
+
+xgettext --no-location lektor_creative_commons/plugin.py --join-existing --output $POT_FILE
+
+for file in $(find . -name '*.po') ; do
+    msgmerge --quiet --no-location --backup=none --update $file $POT_FILE
+done
