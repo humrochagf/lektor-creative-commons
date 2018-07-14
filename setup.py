45b791d80a3be8fb101d0efd29963d8cd1b5fdad
Humberto Rocha <humrochagf@gmail.com>
2018-07-13 23:51:22 -0300

    Force pypi reload

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     description='Lektor plugin to add Creative Commons license to your pages',
     long_description=README,
     long_description_content_type='text/markdown',
-    version='0.5.1',
+    version='0.5.2',
     url='https://github.com/humrochagf/lektor-creative-commons',
     project_urls={
         'Documentation': (
