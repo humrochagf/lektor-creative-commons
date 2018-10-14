9891d1f4b94fd78d975780af7d722d9e6d4ba43c
Humberto Rocha <humrochagf@gmail.com>
2018-10-14 00:11:15 -0300

    Version bump

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     description='Lektor plugin to add Creative Commons license to your pages',
     long_description=README,
     long_description_content_type='text/markdown',
-    version='0.5.3',
+    version='0.6.0',
     url='https://github.com/humrochagf/lektor-creative-commons',
     project_urls={
         'Documentation': (
